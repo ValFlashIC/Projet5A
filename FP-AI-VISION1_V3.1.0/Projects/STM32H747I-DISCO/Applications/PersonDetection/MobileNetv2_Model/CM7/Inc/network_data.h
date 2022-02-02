38c4e5cbbb9bb095ceffa5d146d2ae0563fac73c
Valentin Isaac--Chassande <valflashic@gmail.com>
2022-02-02 11:12:49 +0100

    Revert "Importation Code XCubeAI"
    This reverts commit 4be661b6d3b7b26620924257053e4b26ebf23421.

--- FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/CM7/Inc/network_data.h
+++ FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/CM7/Inc/network_data.h
@@ -3,7 +3,7 @@
   ******************************************************************************
   * @file    network_data.h
   * @author  AST Embedded Analytics Research Platform
-  * @date    Wed Feb  2 10:38:56 2022
+  * @date    Thu Jan  6 13:52:54 2022
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2017 STMicroelectronics.
@@ -34,10 +34,10 @@
 
 
 #define AI_NETWORK_DATA_WEIGHTS_SIZES \
-  { 419196, }
-#define AI_NETWORK_DATA_WEIGHTS_SIZE         (419196)
+  { 412776, }
+#define AI_NETWORK_DATA_WEIGHTS_SIZE         (412776)
 #define AI_NETWORK_DATA_WEIGHTS_COUNT        (1)
-#define AI_NETWORK_DATA_WEIGHT_1_SIZE        (419196)
+#define AI_NETWORK_DATA_WEIGHT_1_SIZE        (412776)
 
 
 
