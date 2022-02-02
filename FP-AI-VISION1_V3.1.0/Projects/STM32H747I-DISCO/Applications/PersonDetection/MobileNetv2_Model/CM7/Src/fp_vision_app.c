3a08842b01ececf99bacc512a5c475c8d5ae7111
Valentin Isaac--Chassande <valflashic@gmail.com>
2022-02-02 11:27:09 +0100

    Init

--- FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/CM7/Src/fp_vision_app.c
+++ FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/CM7/Src/fp_vision_app.c
@@ -122,7 +122,7 @@ AppContext_TypeDef App_Context;
 
 float nn_output_buff[AI_NET_OUTPUT_SIZE] = {0};
  
-const char* output_labels[AI_NET_OUTPUT_SIZE] = {"Boissons", "Bureau", "Claviers", "Plats", "Sacs", "Tables", "Telecommandes"};
+const char* output_labels[AI_NET_OUTPUT_SIZE] = {"Not-person", "Person"};
 
 /* Private function prototypes -----------------------------------------------*/
 static void CameraCaptureBuff2LcdBuff_Copy(AppContext_TypeDef *);
