a5aa480e1552c2fa192c7ee7eefed1b685d77969
Valentin Isaac--Chassande <valflashic@gmail.com>
2022-02-16 10:06:07 +0100

    Init projet PersonDetect

--- FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/STM32CubeIDE/CM7/PersonDetect_MobileNetv2/Application/STM32CubeIDE/startup_cm4.d
+++
@@ -1,2 +0,0 @@
-Application/STM32CubeIDE/startup_cm4.o: \
- D:/Flash/Documents/Git/Projet5A/FP-AI-VISION1_V3.1.0/Projects/STM32H747I-DISCO/Applications/PersonDetection/MobileNetv2_Model/STM32CubeIDE/startup_cm4.s
